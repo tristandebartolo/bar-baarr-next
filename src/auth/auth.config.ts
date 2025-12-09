f6fe3fa4c75472ad4345356dd3afc71ed7ffa3dc
Tristan Code <tristandebartolo@gmail.com>
2025-12-09 19:09:33 +0100

    Test prod path redirect

--- src/auth/auth.config.ts
+++ src/auth/auth.config.ts
@@ -12,21 +12,25 @@ export const authConfig = {
       const isLoggedIn = !!auth?.user;
       let pathname = "/";
 
+      console.log('new URL(url).pathname', new URL(url).pathname)
+
       try {
         // Cas normal : request.url est une URL valide
         pathname = new URL(url).pathname;
+        console.log('TRY', pathname)
       } catch {
         // Cas Vercel preview pourri : request.url = "//xxxx.vercel.app:3000/fr/club"
         // ou nextUrl.pathname contient le double slash + port
         const raw = nextUrl.pathname || url || "";
         pathname = raw
-          .replace(/^\/\/[^\/]+(:\d+)?/, "") // supprime //domaine:3000
+          .replace(/^\/\/[^\/]+(?::\d+)?/, "") // supprime //domaine:3000
           .split("?")[0]
           .split("#")[0] || "/";
+        console.log('CATCH', pathname)
       }
 
       console.log('currentHost pathname', pathname)
-      console.log('currentHost pathname', nextUrl.pathname)
+      console.log('currentHost nextUrl.pathname', nextUrl.pathname)
 
 
       const isOnDashboard = pathname.startsWith("/fr/dashboard");
