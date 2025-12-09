e501800f7654fc5a92293579ff22f0489be42f45
Tristan Code <tristandebartolo@gmail.com>
2025-12-09 10:10:27 +0100

    Remove builder folder app

--- public/src/components/Auth/SessionProvider.jsx
+++
@@ -1,8 +0,0 @@
-"use strict";
-"use client";
-Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = require("next-auth/react");
-const SessionWrapper = ({ children }) => {
-    return <react_1.SessionProvider>{children}</react_1.SessionProvider>;
-};
-exports.default = SessionWrapper;
--- public/src/components/ui/CookieBanner/CookiePreferencesButton.jsx
+++
@@ -1,9 +0,0 @@
-"use strict";
-"use client";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = CookiePreferencesButton;
-function CookiePreferencesButton({ onClick }) {
-    return (<button onClick={onClick} className="fixed bottom-6 right-6 z-50 flex items-center gap-2 rounded-full bg-gray-800/90 px-4 py-2.5 text-xs font-medium text-white backdrop-blur-sm transition hover:bg-gray-700" aria-label="Préférences cookies">
-      Cookies
-    </button>);
-}
